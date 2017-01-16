58b766c6e43b92834ff94a64619f651e2d8f7841
sovedeckic <vmulic2@etf.unsa.ba>
2017-01-16 23:12:02 +0100

    committttt

--- DBInfo.php
+++ DBInfo.php
@@ -5,7 +5,7 @@ class DBInfo
   public static $DB_CONNECTION_STRING_OLD="mysql:dbname=wt_creda;host=localhost;charset=utf8;";
   public static $USERNAME_OLD="korisnik";
   public static $PASSWORD_OLD="korisnik";
-  public static $DB_CONNECTION_STRING='mysql:host=' . getenv('MYSQL_SERVICE_HOST') . ';port=3306;dbname=flights';
+  public static $DB_CONNECTION_STRING='mysql:host=' . getenv('MYSQL_SERVICE_HOST') . ';port=3306;dbname=flightsdb';
   public static $USERNAME="creda";
   public static $PASSWORD="asvordpanjo";
   public static function getMesssageForErrorCode($errCode){
